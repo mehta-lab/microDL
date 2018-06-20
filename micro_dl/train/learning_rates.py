c6118a8125ef32bbfe2841e5752845675e8e0c70
Jenny <jenny.folkesson@gmail.com>
2018-06-19 21:10:30 -0700

    feedback

--- micro_dl/train/learning_rates.py
+++ micro_dl/train/learning_rates.py
@@ -6,11 +6,16 @@ from keras import backend as K
 
 class CyclicLearning(Callback):
     """
-    Custom Callback implementing cyclical learning rate (CLR)
-    as in the paper: https://arxiv.org/abs/1506.01186.
+    Custom Callback implementing cyclical learning rate (CLR) as in the paper:
+    https://arxiv.org/abs/1506.01186.
 
-    Learning rate is increased then decreased in a repeated
-    triangular pattern over time. One triangle = one cycle.
+    Learning rate is increased then decreased in a repeated triangular
+    pattern over time. One triangle = one cycle.
+    step-size is the number of iterations / batches in half a cycle.
+    The paper recommends a step-size of 2-10 times the number of batches in
+    an epoch (empirical) i.e step-size = 2-10 epochs.
+    Also best to stop training at the end of a cycle when the learning rate is
+    at minimum value and accuracy/performance potentially peaks.
     Initial amplitude is scaled by gamma ** iterations.
     https://keras.io/callbacks/
     https://github.com/bckenstler/CLR
@@ -27,17 +32,18 @@ class CyclicLearning(Callback):
         :param float base_lr: Base (minimum) learning rate
         :param float max_lr: Maximum learning rate
         :param float step_size: The number of iterations per half cycle
+            in epochs
         :param float gamma: Constant factor for max_lr exponential
             decrease over time (gamma ** iterations) gamma [0, 1]
-        :param str scale_mode: Evaluate scaling on cycle number
-            ('cycle' - default) or cycle iteration ('iterations')
+        :param str scale_mode: Evaluate scaling on 'cycle' (default) or
+            'iterations'
         """
         super(CyclicLearning, self).__init__()
 
         self.base_lr = base_lr
         self.max_lr = max_lr
-        self.step_size = step_size
-        assert gamma >= 0. and gamma <=1., \
+        self.epoch_step_size = step_size
+        assert 0 <= gamma <= 1., \
             "Gamma is {}, should be [0, 1]".format(gamma)
         self.gamma = gamma
         self.iterations = 0.
@@ -51,13 +57,18 @@ class CyclicLearning(Callback):
 
         :return float clr: Learning rate as a function of iterations
         """
-        cycle = np.floor(1 + self.iterations / (2 * self.step_size))
-        x = np.abs(self.iterations / self.step_size - 2 * cycle + 1)
-        scale_factor = (self.max_lr - self.base_lr) * np.maximum(0, (1 - x))
+        # params from super class contains steps, which is the same as
+        # iterations from paper
+        iter_step_size = self.epoch_step_size * self.params['steps']
+        # Number of cycles
+        cycle_nbr = np.floor(1 + self.iterations / (2 * iter_step_size))
+        # Compute where in the cycle you are and compute local learning rate
+        x = np.abs(self.iterations / iter_step_size - 2 * cycle_nbr + 1)
+        local_lr = (self.max_lr - self.base_lr) * np.maximum(0, (1 - x))
         if self.scale_mode == 'cycle':
-            return self.base_lr + scale_factor * (self.gamma ** cycle)
+            return self.base_lr + local_lr * (self.gamma ** cycle_nbr)
         else:
-            return self.base_lr + scale_factor * (self.gamma ** self.iterations)
+            return self.base_lr + local_lr * (self.gamma ** self.iterations)
 
     def on_train_begin(self, logs=None):
         """
