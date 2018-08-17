ff68b468ed2f08c3d818836dd39e5288f9fb0451
anithapriya <anithapriya@gmail.com>
2018-08-17 11:45:52 -0700

    Revert "refactored networks, to be debugged"
    This reverts commit d3351fd731f5442afb8de311c0ee152b8cdb0e28.

--- micro_dl/networks/layers/__init__.py
+++
@@ -1,4 +0,0 @@
-"""Classes related to layers of a NN"""
-
-from micro_dl.networks.layers.interp_upsampling2D import InterpUpSampling2D
-from micro_dl.networks.layers.interp_upsampling3D import InterpUpSampling3D
