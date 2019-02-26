45e2fafc38a7a26f2326b96f2ef9dcc5cf722447
anithapriya <anithapriya@gmail.com>
2019-02-25 16:56:27 -0800

    removed specific imports in train module

--- micro_dl/train/__init__.py
+++ micro_dl/train/__init__.py
@@ -1,6 +1,2 @@
 """Module for train functions"""
-from .learning_rates import CyclicLearning
-from .lr_finder import LRFinder
-from .model_inference import ModelEvaluator
-from .trainer import BaseKerasTrainer
 
