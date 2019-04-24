0fdbb01ab425876b7fe026d5509955ef9ca1fb2b
anithapriya <anithapriya@gmail.com>
2019-04-24 09:50:45 -0700

    fix mask in estimate metrics, revert zxy to zyx

--- micro_dl/preprocessing/tile_nonuniform_images.py
+++ micro_dl/preprocessing/tile_nonuniform_images.py
@@ -24,7 +24,7 @@ class ImageTilerNonUniform(ImageTilerUniform):
                  pos_ids=-1,
                  hist_clip_limits=None,
                  flat_field_dir=None,
-                 image_format='zxy',
+                 image_format='zyx',
                  num_workers=4,
                  int2str_len=3):
         """Init
