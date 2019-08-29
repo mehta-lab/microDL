06fcec554e8c00d4335439553bc790ae2d91988b
pranathi.vemuri <pranathi93.vemuri@gmail.com>
2019-08-29 16:52:18 +0000

    pandas raises error for time idx columns otherwise

--- micro_dl/train/metrics.py
+++ micro_dl/train/metrics.py
@@ -17,6 +17,13 @@ def coeff_determination(y_true, y_pred):
     return 1 - ss_res / (ss_tot + K.epsilon())
 
 
+def binary_accuracy(y_true, y_pred):
+    '''Calculates the mean accuracy rate across all predictions for binary
+    classification problems.
+    '''
+    return K.mean(K.equal(y_true, K.round(y_pred)))
+
+
 def mask_accuracy(n_channels):
     """split y_true into y_true and mask
 
@@ -38,7 +45,7 @@ def mask_accuracy(n_channels):
         else:
             split_axis = 1
         y_true_split, mask = tf.split(y_true, [n_channels, 1], axis=split_axis)
-        a = K.binary_accuracy(y_true_split, y_pred)
+        a = binary_accuracy(y_true_split, y_pred)
         return a
     return acc
 
