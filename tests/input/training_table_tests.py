f908d2cf85a58c63267b37aad536041ed1f538d9
Jenny <jenny.folkesson@gmail.com>
2019-09-30 14:13:21 -0700

    moved split train val test, added training table tests

--- tests/input/training_table_tests.py
+++ tests/input/training_table_tests.py
@@ -38,8 +38,6 @@ class TestTrainingTable(unittest.TestCase):
                             ignore_index=True,
                         )
         self.tiles_meta = aux_utils.sort_meta_by_channel(self.frames_meta)
-        print(self.tiles_meta.head())
-
         self.input_channels = [0, 2]
         self.target_channels = [3]
         self.mask_channels = [1]
@@ -84,3 +82,94 @@ class TestTrainingTable(unittest.TestCase):
     def test_get_col_name(self):
         col_names = self.table_inst._get_col_name([1, 3])
         self.assertListEqual(col_names, ['file_name_1', 'file_name_3'])
+
+    def test_get_df(self):
+        retain_columns = ['channel_idx',
+                          'pos_idx',
+                          'time_idx',
+                          'slice_idx',
+                          'fpaths_input',
+                          'fpaths_target',
+                          'fpaths_mask']
+        concat_df = self.table_inst._get_df(
+            cur_df=self.tiles_meta,
+            retain_columns=retain_columns,
+        )
+        self.assertListEqual(list(concat_df), retain_columns)
+        # channel_idx is useless once the df is concatenated by channels
+        self.assertListEqual(
+            concat_df['channel_idx'].unique().tolist(),
+            [0],
+        )
+        self.assertListEqual(
+            concat_df['time_idx'].unique().tolist(),
+            self.time_ids,
+        )
+        self.assertListEqual(
+            concat_df['slice_idx'].unique().tolist(),
+            self.slice_ids,
+        )
+        self.assertListEqual(
+            concat_df['time_idx'].unique().tolist(),
+            self.time_ids,
+        )
+        self.assertListEqual(
+            concat_df['pos_idx'].unique().tolist(),
+            self.pos_ids,
+        )
+        # Validate first row of file paths
+        row0 = concat_df.iloc[0]
+        self.assertEqual(
+            row0['fpaths_input'],
+            'im_c000_z000_t003_p007.png,im_c002_z000_t003_p007.png',
+        )
+        self.assertEqual(
+            row0['fpaths_target'],
+            'im_c003_z000_t003_p007.png',
+        )
+        self.assertEqual(
+            row0['fpaths_mask'],
+            'im_c001_z000_t003_p007.png',
+        )
+
+    def test_get_df_subset(self):
+        subset_df = self.table_inst._get_df_subset([10, 15])
+        self.assertListEqual(
+            subset_df['pos_idx'].unique().tolist(),
+            [10, 15],
+        )
+        # Should be 2/5 of dataset
+        self.assertEqual(subset_df.shape[0] / self.tiles_meta.shape[0], 0.4)
+
+    def test_get_df_subset_index(self):
+        self.table_inst.split_by_column = 'index'
+        subset_df = self.table_inst._get_df_subset([10, 15, 20])
+        self.assertListEqual(
+            subset_df.index.values.tolist(),
+            ['10', '15', '20'],
+        )
+        # Should be 2/5 of dataset
+        self.assertEqual(subset_df.shape[0], 3)
+
+    def test_split_train_val_test(self):
+        split_idx = self.table_inst.split_train_val_test(
+            sample_set=self.pos_ids,
+            train_ratio=0.4,
+            test_ratio=0.4,
+            val_ratio=0.2,
+        )
+        self.assertEqual(len(split_idx['train']), 2)
+        self.assertEqual(len(split_idx['val']), 1)
+        self.assertEqual(len(split_idx['test']), 2)
+
+    def test_train_test_split(self):
+        df_dict, split_idx = self.table_inst.train_test_split()
+        # Split train/val/test .6/.2/.2 -> 3/1/1
+        self.assertEqual(len(split_idx['train']), 3)
+        self.assertEqual(len(split_idx['val']), 1)
+        self.assertEqual(len(split_idx['test']), 1)
+        # Assert tables are the right size
+        total_samples = self.tiles_meta.shape[0]
+        self.assertEqual(df_dict['df_train'].shape[0] / total_samples, 0.6)
+        self.assertEqual(df_dict['df_val'].shape[0] / total_samples, 0.2)
+        self.assertEqual(df_dict['df_test'].shape[0] / total_samples, 0.2)
