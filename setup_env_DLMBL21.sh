98dea0a3371eaed43077402afaece5606b04fc74
mattersoflight <shalin.mehta@czbiohub.org>
2021-09-26 04:19:10 +0000

    edited the image translation exercies for clarity, added TOC, and added jupyterlab to conda environment config.

--- setup_env_DLMBL21.sh
+++ setup_env_DLMBL21.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-# checkout the correct branch
-git checkout dl_mbl_2021
-
 # create an environment - can take time, should be run at the start of image translation exercise.
 conda env create --file=conda_environment.yml
 conda init bash
