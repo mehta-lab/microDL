99e1285579bb1f477a8eb0aa2082cb3801b9ec1b
Jenny Folkesson <jenny.folkesson@gmail.com>
2022-10-20 13:15:28 -0700

    finished test zarr reader script

--- tests/test_zarr_reading.py
+++ tests/test_zarr_reading.py
@@ -6,7 +6,9 @@ metadata are the same from both inputs:
 
 from copy import deepcopy
 import numpy as np
+import numpy.testing
 import pandas as pd
+import nose.tools
 import os
 import shutil
 import unittest
@@ -16,6 +18,7 @@ import time
 
 import micro_dl.preprocessing.estimate_flat_field as flat_field
 import micro_dl.utils.aux_utils as aux_utils
+import micro_dl.utils.image_utils as im_utils
 import micro_dl.cli.preprocess_script as preprocess_script
 
 
@@ -52,76 +55,132 @@ def parse_args():
     return args
 
 
-class TestZarrReading(unittest.TestCase):
+def get_meta_idx(frames_metadata,
+                 time_idx,
+                 channel_idx,
+                 slice_idx,
+                 pos_idx,
+                 row_start,
+                 col_start):
+    """
+    Get row index in metadata dataframe given variable indices
 
-    def setUp(self):
-        """
-        Write .yml files for preprocessing with .zarr and .tif from the same directory
-        Specify a directory of .zarr files.
-        """
-        self.zarr_tiles = None
-        self.tiff_tiles = None
-    
-    def compare_tiling(self, tiff_tile_dir, zarr_tile_dir):
-        '''
-        Compares .npy files output from preprocessing done through reading zarr and tiff 
-        files. If any .npy files are substantially different, errors
-        
-        :param str tiff_tile_dir: directory containing tiff-preprocessed tiles
-        :param str zarr_tile_dir: directory containing zarr-preprocessed tiles
-        '''
-        tiff_tile_names = self._get_files_of_type(tiff_tile_dir, ftype='.npy')
-        zarr_tile_names = self._get_files_of_type(zarr_tile_dir, ftype='.npy')
-        
-        assert len(tiff_tile_names) == len(zarr_tile_names), 'Different numbers of tiles'
-        
-        for i in range(len(tiff_tile_names)):
-            tiff_tile = np.load(os.path.join(tiff_tile_dir, tiff_tile_names[i]))
-            zarr_tile = np.load(os.path.join(zarr_tile_dir, zarr_tile_names[i]))
-            print(f'Comparing tile {i}/{len(zarr_tile_names)}...', end='\r')
-            time.sleep(0.00001)
-            
-            er_msg = f'Tiles {zarr_tile_names[i]} (zarr) and {tiff_tile_names[i]} (tiff) unequal'
-            np.testing.assert_equal(tiff_tile, zarr_tile, err_msg = er_msg)
-        print('\n', end='\r')
-        print('\t --> compare_tiling passed')
+    :param dataframe frames_metadata: Dataframe with column names given below
+    :param int time_idx: Timepoint index
+    :param int channel_idx: Channel index
+    :param int slice_idx: Slice (z) index
+    :param int pos_idx: Position (FOV) index
+    :return: int pos_idx: Row position matching indices above
+    """
+    frame_idx = frames_metadata.index[
+        (frames_metadata['channel_idx'] == int(channel_idx)) &
+        (frames_metadata['time_idx'] == int(time_idx)) &
+        (frames_metadata["slice_idx"] == int(slice_idx)) &
+        (frames_metadata["pos_idx"] == int(pos_idx)) &
+        (frames_metadata["row_start"] == int(row_start)) &
+        (frames_metadata["col_start"] == int(col_start))
+        ].tolist()
+    return frame_idx[0]
 
-    def compare_metadata(self, tiff_tile_dir, zarr_tile_dir):
-        '''
-        Compares metadata files output from preprocessing done through reading zarr and tiff 
-        data storage. If metadata is different, errors.
-        
-        :param str tiff_tile_dir: directory containing tiff-preprocessed tiles
-        :param str zarr_tile_dir: directory containing zarr-preprocessed tiles
-        '''
-        tiff_meta_files = self._get_files_of_type(tiff_tile_dir, ftype='.csv')
-        zarr_meta_files = self._get_files_of_type(zarr_tile_dir, ftype='.csv')
-        for i in range(len(tiff_meta_files)):
-            tiff_meta = pd.read_csv(tiff_meta_files[i])
-            zarr_meta = pd.read_csv(zarr_meta_files[i])
-            print(f'Comparing metadata {i+1}/{len(tiff_meta_files)}...', end='\r')
-            # Remove directories since they're not the same
-            tiff_meta.drop(['dir_name'], axis=1)
-            zarr_meta.drop(['dir_name'], axis=1)
-            try:
-                tiff_meta.compare(zarr_meta)
-            except:
-                er_msg = f'Metadata {zarr_meta_files[i]} (zarr) and {tiff_meta_files[i]} (tiff) unequal'
-                raise AssertionError(er_msg)
-        print('\n', end='\r')
-        print('\t --> compare_metadata passed')
+
+def get_channel_correspondence(tiff_config, zarr_config):
+    """
+    Get channels for each data type since indices differ
+    """
+    tiff_map = tiff_config['channel_map']
+    zarr_map = zarr_config['channel_map']
+    # Map tiff channel ids, mask channel (3) is same
+    zarr_ids = [3]
+    tiff_ids = [3]
+    for tiff_name in list(tiff_map):
+        zarr_name = [s for s in list(zarr_map) if tiff_name in s.lower()][0]
+        print("tiff {} idx: {}, matches zarr {} idx: {}".format(
+            tiff_name,
+            tiff_map[tiff_name],
+            zarr_name,
+            zarr_map[zarr_name],
+        ))
+        zarr_ids.append(int(zarr_map[zarr_name]))
+        tiff_ids.append(int(tiff_map[tiff_name]))
+    return tiff_ids, zarr_ids
+
+
+def compare_flatfields(tiff_dir, zarr_dir, tiff_ids, zarr_ids):
+    """
+    Find corresponding flat field images and compare them
+    """
+    for tiff_channel_idx in tiff_ids:
+        tiff_ff_path = im_utils.get_flat_field_path(
+            tiff_dir,
+            tiff_channel_idx,
+            tiff_ids,
+        )
+        if tiff_ff_path is not None:
+            mapped_idx = tiff_ids.index(tiff_channel_idx)
+            zarr_channel_idx = zarr_ids[mapped_idx]
+            zarr_ff_path = im_utils.get_flat_field_path(
+                zarr_dir,
+                zarr_channel_idx,
+                tiff_ids,
+            )
+            assert zarr_ff_path is not None, \
+                "No corresponding ff for tiff idx {}".format(tiff_channel_idx)
+            # Load and compare
+            print("Comparing tiff ff {} with zarr {}".format(
+                tiff_ff_path,
+                zarr_ff_path,
+            ))
+            tiff_ff = np.load(tiff_ff_path)
+            zarr_ff = np.load(zarr_ff_path)
+            numpy.testing.assert_array_equal(tiff_ff, zarr_ff)
+
     
-    def _get_files_of_type(self, root_dir, ftype='.csv'):
-        '''
-        Recursively search for and return all filenames of type 'ftype' in
-        directory or subdirectories of 'root_dir'
-        :return list all_csv_files: List of all files
-        '''
-        PATH = root_dir
-        EXT = '*' + ftype
-        all_csv_files = [y for x in os.walk(PATH) for y in glob.glob(os.path.join(x[0], EXT))]
-        all_csv_files.sort(key=str)
-        return all_csv_files
+def compare_tiling(tiff_config, zarr_config, tiff_ids, zarr_ids):
+    '''
+    Compares .npy files output from preprocessing done through reading zarr and tiff
+    files. First load metdata csv file to match up channel indices.
+    If any .npy files are substantially different, errors
+
+    :param str tiff_config: Config after preprocessing tiff
+    :param str zarr_config: Config after preprocessing zarr
+    '''
+    tiff_tile_dir = tiff_config['tile']['tile_dir']
+    zarr_tile_dir = zarr_config['tile']['tile_dir']
+    nose.tools.assert_equal(
+        len(os.listdir(tiff_tile_dir)),
+        len(os.listdir(zarr_tile_dir)),
+    )
+    # Read metadata
+    tiff_meta = aux_utils.read_meta(tiff_tile_dir)
+    tiff_meta.reset_index(drop=True, inplace=True)
+    zarr_meta = aux_utils.read_meta(zarr_tile_dir)
+    zarr_meta.reset_index(drop=True, inplace=True)
+    # Check that shape is the same
+    nose.tools.assert_equal(tiff_meta.shape, zarr_meta.shape)
+
+    for idx, tiff_row in tiff_meta.iterrows():
+        tiff_channel_idx = tiff_row['channel_idx']
+        mapped_idx = tiff_ids.index(tiff_channel_idx)
+        zarr_channel_idx = zarr_ids[mapped_idx]
+        zarr_idx = get_meta_idx(
+            frames_metadata=zarr_meta,
+            time_idx=tiff_row['time_idx'],
+            channel_idx=zarr_channel_idx,
+            slice_idx=tiff_row['slice_idx'],
+            pos_idx=tiff_row['pos_idx'],
+            row_start=tiff_row['row_start'],
+            col_start=tiff_row['col_start'],
+        )
+        zarr_tile_name = zarr_meta.loc[zarr_idx, 'file_name']
+        print("{}. Comparing tiff: {} with zarr: {}".format(
+            idx,
+            tiff_row['file_name'],
+            zarr_tile_name,
+        ))
+        zarr_tile = np.load(os.path.join(zarr_tile_dir, zarr_tile_name))
+        tiff_tile = np.load(os.path.join(tiff_tile_dir, tiff_row['file_name']))
+        # Check that all tiles are identical
+        numpy.testing.assert_array_equal(zarr_tile, tiff_tile)
 
 
 def get_base_preprocess_config():
@@ -192,29 +251,48 @@ def main(zarr_dir=None, tiff_dir=None, output_dir=None, config_path=None):
     # generate tiles using tiff and zarr
     if not os.path.exists(zarr_preprocess_config['output_dir']):
         print('\t Running zarr preprocessing...',end='')
-        zarr_prep_config, runtime = preprocess_script.pre_process(zarr_preprocess_config)
+        zarr_out_config, runtime = preprocess_script.pre_process(zarr_preprocess_config)
         print('Done. Time: {} s'.format(runtime))
+        file_name = os.path.join(zarr_preprocess_config['output_dir'], 'preprocess_config.json')
+        aux_utils.write_json(zarr_out_config, file_name)
     if not os.path.exists(tiff_preprocess_config['output_dir']):
         print('\t Running tiff preprocessing ...',end='')
-        tiff_prep_config, runtime = preprocess_script.pre_process(tiff_preprocess_config)
+        tiff_out_config, runtime = preprocess_script.pre_process(tiff_preprocess_config)
         print('Done. Time: {} s'.format(runtime))
+        file_name = os.path.join(tiff_preprocess_config['output_dir'], 'preprocess_config.json')
+        aux_utils.write_json(tiff_out_config, file_name)
     
-    # initiate tester and run tests
-    tester = TestZarrReading()
-    
-    try:
-        print('Running tests...')
-        # compare methods
-        tester.compare_tiling(tiff_prep_config['output_dir'], zarr_prep_config['output_dir'])
-        tester.compare_metadata(tiff_prep_config['output_dir'], zarr_prep_config['output_dir'])
-        
-        # # cleanup directories if pass
-        # shutil.rmtree(tiff_prep_config['output_dir'])
-        # shutil.rmtree(zarr_prep_config['output_dir'])
-        
-    except Exception as e:
-        print(e.args)
-    
+    # Run tests
+    print('Running tests on tiles metadata and files')
+    # Get output config files
+    file_name = os.path.join(zarr_preprocess_config['output_dir'], 'preprocess_config.json')
+    zarr_out_config = aux_utils.read_json(file_name)
+    file_name = os.path.join(tiff_preprocess_config['output_dir'], 'preprocess_config.json')
+    tiff_out_config = aux_utils.read_json(file_name)
+
+    tiff_ids, zarr_ids = get_channel_correspondence(
+        tiff_out_config,
+        zarr_out_config,
+    )
+    # Compare flat field images
+    compare_flatfields(
+        tiff_out_config['flat_field']['flat_field_dir'],
+        zarr_out_config['flat_field']['flat_field_dir'],
+        tiff_ids,
+        zarr_ids,
+    )
+    # Compare tiles between data formats
+    compare_tiling(
+        tiff_out_config,
+        zarr_out_config,
+        tiff_ids,
+        zarr_ids,
+    )
+    print('Tests completed.')
+    # # cleanup directories if pass
+    # shutil.rmtree(tiff_prep_config['output_dir'])
+    # shutil.rmtree(zarr_prep_config['output_dir'])
+
 
 if __name__ == '__main__':
     args = parse_args()
