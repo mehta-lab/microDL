fixed shearing bug and added channel selection feature to speed up computation

micro_dl/input/gunpowder_nodes.py
@@ -49,6 +49,7 @@ class ShearAugment(gp.BatchFilter):
         array=None,
         angle_range=(0, 0),
         prob=0.1,
+        shear_middle_slice_only=None,
     ):
         """
         Custom gunpowder augmentation node for applying shear in xy.
@@ -66,6 +67,9 @@ class ShearAugment(gp.BatchFilter):
         :param tuple(float, float) angle_range: range of angles in degrees of shear. To prevent
                                             data corruption, angle must be within (0,30)
         :param float prob: probability of applying shear
+        :param tuple(int)/None shear_middle_slice_only: shear only the middle z-slice of these
+                                                    channel indices. Used when target plucked
+                                                    from stack. By default None.
         """
         assert (
             abs(angle_range[0]) <= 30 and abs(angle_range[1]) <= 30
@@ -75,6 +79,7 @@ class ShearAugment(gp.BatchFilter):
         self.array_keys = array
         self.angle_range = angle_range
         self.prob = prob
+        self.shear_middle_slice_only = shear_middle_slice_only
 
     def prepare(self, request: gp.BatchRequest):
         """
@@ -144,6 +149,9 @@ class ShearAugment(gp.BatchFilter):
                 batch_data = batch[key].data
                 roi = request[key].roi
 
+                if self.shear_middle_slice_only == None:
+                    self.shear_middle_slice_only = ()
+
                 output_shape = list(batch_data.shape[:-3]) + list(
                     request[key].roi.get_shape()
                 )
@@ -153,15 +161,26 @@ class ShearAugment(gp.BatchFilter):
                 #      break in loops. Can be safely implemented with dynamic recursion.
                 for batch_idx in range(batch_data.shape[0]):
                     for channel_idx in range(batch_data.shape[1]):
+                        middle_only = channel_idx in self.shear_middle_slice_only
                         data = batch_data[batch_idx, channel_idx]
+
                         if roi.dims() == 2:
                             data = np.expand_dims(data, 0)
                             data = apply_affine_transform(data, shear=self.angle)[0]
                         else:
-                            data = apply_affine_transform(data, shear=self.angle)
+                            if middle_only:
+                                middle_idx = data.shape[0] // 2
+                                data[middle_idx] = apply_affine_transform(
+                                    np.expand_dims(data[middle_idx], 0),
+                                    shear=self.angle,
+                                )[0]
+                            else:
+                                data = apply_affine_transform(data, shear=self.angle)
 
                         if self.angle > 0:
-                            data = data[..., :, : -self.extra_pixels * 2]
+                            data = data[
+                                ..., :, : data.shape[-1] - self.extra_pixels * 2
+                            ]
                         else:
                             data = data[..., :, self.extra_pixels * 2 :]
 

micro_dl/scripts/gunpowder_pipeline_prototyping.py
@@ -242,19 +242,21 @@ elastic_aug = gp.ElasticAugment(
     rotation_interval=(0, 0),
     scale_interval=(1, 1),
     spatial_dims=2,
+    subsample=1,
 )
 blur_aug = BlurAugment(
     array=raw,
     mode="gaussian",
     width_range=(23, 25),
     sigma=1,
-    prob=0.5,
+    prob=1,
     blur_channels=(0,),
 )
 shear_aug = ShearAugment(
     array=raw,
-    angle_range=(-30, 30),
-    prob=0.33,
+    angle_range=(29, 30),
+    prob=1,
+    # shear_middle_slice_only=(1, 2),
 )
 intensity_aug = IntensityAugment(
     array=raw,
@@ -263,18 +265,20 @@ intensity_aug = IntensityAugment(
     shift_range=(-0.15, 0.15),
     scale_range=(0.5, 1.5),
     norm_before_shift=True,
+    prob=1,
 )
 noise_aug = NoiseAugment(
     array=raw,
     noise_channels=(0,),
     mode="gaussian",
-    prob=0.5,
+    prob=1,
 )
 
 profiling = gp.PrintProfilingStats(every=1)
 
 cache = gp.PreCache(cache_size=2500, num_workers=16)
-batch_stack = gp.Stack(3)
+stack_num = 15
+batch_stack = gp.Stack(stack_num)
 print("done")
 print("building pipeline...", end="")
 batch_pipeline = gpsum(
@@ -288,15 +292,15 @@ batch_pipeline = gpsum(
         blur_aug,
         intensity_aug,
         noise_aug,
-        cache,  # important to cache upstream of stack
+        # cache,  # important to cache upstream of stack
         batch_stack,
     ],
-    track_nodes=False,
-    time_nodes=False,
+    track_nodes=True,
+    time_nodes=True,
 )
 
 request = gp.BatchRequest()
-request[raw] = gp.Roi((15, 728, 728), (5, 256, 256))
+request[raw] = gp.Roi((15, 728, 728), (5, 624, 624))
 
 with gp.build(batch_pipeline) as pipeline:
     print("done")
@@ -315,8 +319,8 @@ with gp.build(batch_pipeline) as pipeline:
     print(f"max: {np.max(data)}, min: {np.min(data)}")
 
     vmax = None
-    fig, ax = plt.subplots(3, 3, figsize=(15, 15))
-    for row in range(3):
+    fig, ax = plt.subplots(stack_num, 3, figsize=(15, 5 * stack_num))
+    for row in range(min(5, stack_num)):
         for channel in range(3):
             if channel == 0:
                 ax[row][channel].imshow(np.mean(data[row][channel], 0), cmap="gray")
@@ -325,10 +329,10 @@ with gp.build(batch_pipeline) as pipeline:
                     f"min:{np.max(np.mean(data[row][channel],0)):.3f}"
                 )
             else:
-                ax[row][channel].imshow(data[row][channel][3])
+                ax[row][channel].imshow(data[row][channel][2])
                 ax[row][channel].set_title(
-                    f"max:{np.max(data[row][channel][3]):.3f}, "
-                    f"min:{np.max(data[row][channel][3]):.3f}"
+                    f"max:{np.max(data[row][channel][2]):.3f}, "
+                    f"min:{np.max(data[row][channel][2]):.3f}"
                 )
     plt.show()
 # %%