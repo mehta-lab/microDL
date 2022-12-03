Fixed issues with array slicing, normalization working on one-position dataset

micro_dl/preprocessing/estimate_flat_field.py
@@ -118,7 +118,7 @@ class FlatFieldEstimator2D:
             all_channels_array.append(flatfield)
 
         all_channels_array = np.stack(all_channels_array, 0)
-        all_channels_array = np.expand_dims(all_channels_array, (0, 1))
+        all_channels_array = np.expand_dims(all_channels_array, (0, 2))
 
         # record flat_field inside zarr store.
         for position in self.modifier.position_map:

micro_dl/utils/image_utils.py
@@ -149,7 +149,8 @@ def resize_mask(input_image, target_size):
 
 
 def apply_flat_field_correction(input_image, **kwargs):
-    """Apply flat field correction.
+    """Apply flat field correction. Input image and flatfield must be
+    provided as 2d images.
 
     :param np.array input_image: image to be corrected
     Kwargs, either:
@@ -157,6 +158,7 @@ def apply_flat_field_correction(input_image, **kwargs):
         flat_field_path (str): Full path to flatfield image
     :return: np.array (float) corrected image
     """
+
     corrected_image = input_image.astype("float")
     if "flat_field_image" in kwargs:
         flat_field_im = kwargs["flat_field_image"]

micro_dl/utils/meta_utils.py
@@ -169,35 +169,43 @@ def generate_normalization_metadata(
     elif isinstance(channel_ids, int):
         channel_ids = [channel_ids]
 
+    # get arguments for multiprocessed grid sampling
     mp_grid_sampler_args = []
     for position in modifier.position_map:
         ff_name = None
+        ff_channels = None
 
         position_metadata = modifier.get_position_meta(position)
-        if "flat_field" in position_metadata:
-            ff_name = position_metadata["flat_field"]["array_name"]
+        if "flatfield" in position_metadata:
+            ff_name = position_metadata["flatfield"]["array_name"]
+            ff_channels = position_metadata["flatfield"]["channel_ids"]
 
-        mp_grid_sampler_args.append((position, ff_name, zarr_dir, grid_spacing))
+        mp_grid_sampler_args.append(
+            [position, ff_name, ff_channels, zarr_dir, grid_spacing]
+        )
 
+    # sample values and use them to get normalization statistics
     for channel in channel_ids:
+        channel_name = modifier.channel_names[channel]
+        this_channels_args = tuple([args + [channel] for args in mp_grid_sampler_args])
+
         # NOTE: Doing sequential mp with pool execution creates synchronization
         #      points between each step. This could be detrimental to performance
         fov_sample_values = mp_utils.mp_sample_im_pixels(
-            mp_grid_sampler_args, num_workers
+            this_channels_args, num_workers
         )
         dataset_sample_values = np.stack(fov_sample_values, 0)
 
-        # get statistics
         fov_level_statistics = mp_utils.mp_get_val_stats(fov_sample_values, num_workers)
-        dataset_level_statistiscs = mp_utils.get_val_stats(dataset_sample_values)
+        dataset_level_statistics = mp_utils.get_val_stats(dataset_sample_values)
 
         for position in modifier.position_map:
             position_statistics = {
                 "fov_statistics": fov_level_statistics[position],
-                "dataset_statistics": dataset_level_statistiscs[position],
+                "dataset_statistics": dataset_level_statistics,
             }
             channel_position_statistics = {
-                channel: position_statistics,
+                channel_name: position_statistics,
             }
 
             modifier.write_meta_field(

micro_dl/utils/mp_utils.py
@@ -568,7 +568,14 @@ def mp_sample_im_pixels(fn_args, workers):
     return list(res)
 
 
-def sample_im_pixels(position, ff_name, zarr_dir, grid_spacing):
+def sample_im_pixels(
+    position,
+    ff_name,
+    ff_channels,
+    zarr_dir,
+    grid_spacing,
+    channel,
+):
     # TODO move out of mp utils into normalization utils
     """
     Read and computes statistics of images for each point in a grid.
@@ -576,24 +583,48 @@ def sample_im_pixels(position, ff_name, zarr_dir, grid_spacing):
     for rows and cols.
     Applies flatfield correction prior to intensity sampling if flatfield
     path is specified.
+    By default, samples from every time position and every z-depth, and
+    assumes that the data in the zarr store is stored in [T,C,Z,Y,X] format,
+    for time, channel, z, y, x.
 
     :param int position: position currently being processed
     :param str ff_name: name of 'untracked' flatfield array
-    :param str zarr_dir: path to zarr directory
+    :param list ff_channels: list of channels with flatfield channel IDs,
+                                only used if a flatfield name is provided
+    :param str zarr_dir: path to HCS-compatible zarr directory
     :param int grid_spacing: spacing of sampling grid in x and y
+    :param int channel: channel to sample from
 
     :return list meta_rows: Dicts with intensity data for each grid point
     """
-    modifier = io_utils.HCSZarrModifier(zarrfile=zarr_dir)
-    im = modifier.get_array(position=position)
+    modifier = io_utils.HCSZarrModifier(zarr_file=zarr_dir)
+    image_zarr = modifier.get_zarr(position=position)
 
-    if ff_name is not None:
-        ff_image = modifier.get_untracked_array(position=position, name=ff_name)
-        im = image_utils.apply_flat_field_correction(
-            input_image=im,
-            flat_field_image=ff_image,
-        )
-    row_indices, col_indices, sample_values = image_utils.grid_sample_pixel_values(
-        im, grid_spacing
-    )
+    flatfield = None
+    if ff_name is not None and channel in ff_channels:
+        flatfield = modifier.get_untracked_array(position=position, name=ff_name)
+
+    all_sample_values = []
+    all_time_indices = list(range(modifier.shape[0]))
+    all_z_indices = list(range(modifier.shape[2]))
+
+    for time_index in all_time_indices:
+        if flatfield is not None:
+            # flatfield array might have collapsed indices
+            ff_channel_pos = ff_channels.index(channel)
+            flatfield_slice = flatfield[time_index, ff_channel_pos, 0, :, :]
+
+        for z_index in all_z_indices:
+            image_slice = image_zarr[time_index, channel, z_index, :, :]
+            if flatfield is not None:
+                image_slice = image_utils.apply_flat_field_correction(
+                    input_image=image_slice,
+                    flat_field_image=flatfield_slice,
+                )
+            _, _, sample_values = image_utils.grid_sample_pixel_values(
+                image_slice, grid_spacing
+            )
+            all_sample_values.append(sample_values)
+    sample_values = np.stack(all_sample_values, 0).flatten()
+
     return sample_values