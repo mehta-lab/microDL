718f3dcc99f48a1bfe618709ed2ccf415ec3e914
Ziwen Liu <67518483+ziw-liu@users.noreply.github.com>
2023-03-31 12:57:48 -0700

    suppress warning

--- micro_dl/light/train.py
+++ micro_dl/light/train.py
@@ -1,3 +1,5 @@
+import warnings
+
 from lightning.pytorch.cli import LightningCLI
 
 from micro_dl.light.data import HCSDataModule
@@ -5,7 +7,11 @@ from micro_dl.light.engine import PhaseToNuc25D
 
 
 def main():
-    _ = LightningCLI(PhaseToNuc25D, HCSDataModule)
+    # TODO: remove this after MONAI 1.2 release
+    # https://github.com/Project-MONAI/MONAI/pull/6105
+    with warnings.catch_warnings():
+        warnings.filterwarnings("ignore", category=UserWarning, message="TypedStorage")
+        _ = LightningCLI(PhaseToNuc25D, HCSDataModule)
 
 
 if __name__ == "__main__":
