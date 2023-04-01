7f5dc9ae253f0c7eca42bf7178aa1be0642ca3e8
Ziwen Liu <67518483+ziw-liu@users.noreply.github.com>
2023-03-31 20:39:52 -0700

    single-threaded blosc decompressing for multi-processing

--- micro_dl/light/train.py
+++ micro_dl/light/train.py
@@ -1,6 +1,7 @@
 import warnings
 
 import torch
+from numcodecs import blosc
 from lightning.pytorch.cli import LightningCLI
 
 from micro_dl.light.data import HCSDataModule
@@ -8,6 +9,8 @@ from micro_dl.light.engine import PhaseToNuc25D
 
 
 def main():
+    # https://zarr.readthedocs.io/en/stable/tutorial.html#configuring-blosc
+    blosc.use_threads = False
     torch.set_float32_matmul_precision("high")
     # TODO: remove this after MONAI 1.2 release
     # https://github.com/Project-MONAI/MONAI/pull/6105
